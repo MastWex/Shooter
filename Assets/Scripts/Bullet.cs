6bbdcde48812308f35aad27c52d3b1e8d5c3a861
MastWex <09dracon@gmail.com>
2024-02-22 17:30:21 +0300

    Bullet_(Save1)

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float Speed;
+    public float lifetime;
+
+    private void Start()
+    {
+        Invoke("DestroyBullet", lifetime);
+    }
+    private void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        DestroyBullet();
+    }
+
+    private void MoveFixedUpdate()
+    {
+        transform.Translate(Vector3.forward * Speed * Time.fixedDeltaTime);
+    }
+
+    private void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public Bullet BulletPrefab;
+    public Transform MuzzleTransform;
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(BulletPrefab, MuzzleTransform.position, MuzzleTransform.rotation);
+        }
+    }
+}
